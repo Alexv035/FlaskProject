f253f71c1905ce533c25fdfe4688b2a939e21873
Alexv035 <voronkov.a.g@gmail.com>
2023-01-07 22:28:12 +0300

    Lesson 2

--- blog/app.py
+++ blog/app.py
@@ -1,3 +1,6 @@
+from blog.views.articles import articles_app
+from blog.views.users import users_app
+from flask import render_template
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,4 +8,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello web!"
+    return render_template("index.html")
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+app.register_blueprint(users_app, url_prefix="/users")
+
+app.register_blueprint(articles_app, url_prefix="/articles")
